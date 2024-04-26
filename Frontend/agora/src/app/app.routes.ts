4a1eecc850caca8cd9042cbede587d5952765244
prazvan10 <pascalrazvanalexandru@gmail.com>
2024-04-26 17:45:19 +0300

    Create routing

--- Frontend/agora/src/app/app.routes.ts
+++ Frontend/agora/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CreatePetitionComponent } from './create-petition/create-petition.component';
+import { ListOfPetitionsComponent } from './list-of-petitions/list-of-petitions.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, 
+    { path: 'Login', component: LoginComponent },
+    { path: 'Petitii', component: ListOfPetitionsComponent },
+    { path: 'Deleaga-votul', component: UsersComponent },
+    { path: 'Creaza-petitie', component: CreatePetitionComponent },
+    { path: 'Creare-cont', component: CreateAccountComponent }
+];
