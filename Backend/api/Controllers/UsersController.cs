3ebe6976bf0a2dd9e94d616617d5ec507a0b289d
prazvan10 <pascalrazvanalexandru@gmail.com>
2024-04-29 14:16:27 +0300

    Create Users api

--- Backend/api/Controllers/UsersController.cs
+++ Backend/api/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 using Microsoft.Data.SqlClient;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using api.Models;
 
 
 namespace api.Controllers
@@ -51,6 +52,76 @@ namespace api.Controllers
         array.Add(itemToAdd);
         }
         jsonToOutput = JsonConvert.SerializeObject(array, Formatting.Indented);
+        return Ok(jsonToOutput);
+        }
+
+                // POST: api/ManageUsers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost(Name = "PostUsers")]
+        public IActionResult PostUserItem(Users userItem)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string sql = "INSERT INTO Users (UserName, UserPassword, FirstName, LastName, Email, Telephone) VALUES (@UserName, @UserPassword, @FirstName, @LastName, @Email, @Telephone)";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@UserName", userItem.UserName);
+            command.Parameters.AddWithValue("@UserPassword", userItem.UserPassword);
+            command.Parameters.AddWithValue("@FirstName", userItem.FirstName);
+            command.Parameters.AddWithValue("@LastName", userItem.LastName);
+            command.Parameters.AddWithValue("@Email", userItem.Email);
+            command.Parameters.AddWithValue("@Telephone", userItem.Telephone);
+            command.ExecuteNonQuery();
+            connection.Close();
+            return Ok();
+        }
+
+        [HttpGet("Username")]
+        public IActionResult GetUserItemByName(string username)
+        {
+        SqlConnection connection = new SqlConnection(connectionString);
+        connection.Open();
+        string sql = "SELECT * FROM Users WHERE UserName=@UserName";
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.Parameters.AddWithValue("@UserName", username);
+        SqlDataReader dataReader = command.ExecuteReader();
+        JObject jsonItem =new JObject();
+        var jsonToOutput = JsonConvert.SerializeObject(jsonItem, Formatting.Indented);
+        while(dataReader.Read())
+        {
+        jsonItem["UserID"] = dataReader["UserID"].ToString();
+        jsonItem["UserName"] = dataReader["UserName"].ToString();
+        jsonItem["UserPassword"] = dataReader["UserPassword"].ToString();
+        jsonItem["FirstName"] = dataReader["FirstName"].ToString();
+        jsonItem["LastName"] = dataReader["LastName"].ToString();
+        jsonItem["Email"] = dataReader["Email"].ToString();
+        jsonItem["Telephone"] = dataReader["Telephone"].ToString();
+        }
+        jsonToOutput = JsonConvert.SerializeObject(jsonItem, Formatting.Indented);
+        return Ok(jsonToOutput);
+        }
+
+        [HttpGet("Id")]
+        public IActionResult GetUserItemById(int id)
+        {
+        SqlConnection connection = new SqlConnection(connectionString);
+        connection.Open();
+        string sql = "SELECT * FROM Users WHERE UserId=@ID";
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.Parameters.AddWithValue("@ID", id);
+        SqlDataReader dataReader = command.ExecuteReader();
+        JObject jsonItem =new JObject();
+        var jsonToOutput = JsonConvert.SerializeObject(jsonItem, Formatting.Indented);
+        while(dataReader.Read())
+        {
+        jsonItem["UserID"] = dataReader["UserID"].ToString();
+        jsonItem["UserName"] = dataReader["UserName"].ToString();
+        jsonItem["UserPassword"] = dataReader["UserPassword"].ToString();
+        jsonItem["FirstName"] = dataReader["FirstName"].ToString();
+        jsonItem["LastName"] = dataReader["LastName"].ToString();
+        jsonItem["Email"] = dataReader["Email"].ToString();
+        jsonItem["Telephone"] = dataReader["Telephone"].ToString();
+        }
+        jsonToOutput = JsonConvert.SerializeObject(jsonItem, Formatting.Indented);
         return Ok(jsonToOutput);
         }
     }
--- Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820c36d0e4ce79a675333ecb2046d8ba109fdde1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249b87d501b57d5f3baa02b7e20fecc75e0e6dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
